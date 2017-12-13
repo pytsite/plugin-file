4918c8a9ceed2b867206267a1950fb3e066ca21e
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:08:56 +0200

    Release 1.2

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 """PytSite File Plugin
 """
-from pytsite import plugman as _plugman
-
-# Public API
-if _plugman.is_installed(__name__):
-    from . import _model as model, _error as error, _driver as driver
-    from ._api import create, get, get_multiple
-
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
+
+from pytsite import plugman as _plugman
+
+if _plugman.is_installed(__name__):
+    # Public API
+    from . import _model as model, _error as error, _driver as driver
+    from ._api import create, get, get_multiple
